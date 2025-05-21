79831e0face3b6c94ee834dc01dfe9efaf6ef946
Markus Cserna <git@cserna.at>
2025-05-21 14:51:02 +0200

    Changed login code as Hargassner seems to have removed the XSRF headers.

--- hargassner/client.py
+++ hargassner/client.py
@@ -23,20 +23,10 @@ class HargassnerAPI:
                 with open(self.authdat, 'rb') as file:
                     data = pickle.load(file)
                     file.close()
-                    return data['xsrf_token'], data['bearer']
+                    return data['bearer']
             except IOError as e:
                 if self.debug: print("auth.dat not present. Forcing login.")
                 pass
-
-        # GET XSRF-TOKEN FROM LOGIN PAGE
-        response = requests.get('https://web.hargassner.at/login')
-        xsrf_token = response.headers['set-cookie']
-        xsrf_header = {
-            'X-XSRF-TOKEN': xsrf_token,
-        }
-        xsrf_token = xsrf_token.split(';')[0]
-
-        if self.debug: print('xsrf-token: ' + xsrf_token)
 
         # LOGIN
         login_data = {
@@ -46,8 +36,7 @@ class HargassnerAPI:
             'client_secret': self.client_secret,
         }
 
-        response = requests.post('https://web.hargassner.at/api/auth/login', headers=xsrf_header, data=login_data,
-                                 verify=True)
+        response = requests.post('https://web.hargassner.at/api/auth/login', data=login_data, verify=True)
         j = response.json()
         bearer = j['access_token']
 
@@ -56,20 +45,19 @@ class HargassnerAPI:
         if self.debug: print('bearer: ' + bearer)
 
         with open(self.authdat, 'wb') as file:
-            data = {'xsrf_token': xsrf_token, 'bearer': bearer}
+            data = {'bearer': bearer}
             pickle.dump(data, file)
             file.close()
 
-        return xsrf_token, bearer
+        return bearer
 
     def query_api(self):
-        xsrf_token, bearer = self.login()
+        bearer = self.login()
 
         # DEVICE QUERY API
         authentication_header = {
             'Accept': 'application/json',
             'Authorization': 'Bearer ' + bearer,
-            'X-XSRF-TOKEN': xsrf_token,
         }
 
         response = requests.get('https://web.hargassner.at/api/installations/' + self.installation + '/widgets',
@@ -80,12 +68,11 @@ class HargassnerAPI:
 
         if 'message' in j and j['message'] == 'Unauthenticated.':
             # We're unauthenticated. Force login and try again.
-            xsrf_token, bearer = self.login(True)
+            bearer = self.login(True)
 
             authentication_header = {
                 'Accept': 'application/json',
                 'Authorization': 'Bearer ' + bearer,
-                'X-XSRF-TOKEN': xsrf_token,
             }
 
             response = requests.get('https://web.hargassner.at/api/installations/' + self.installation + '/widgets',
